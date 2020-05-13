fe146fe3d57a9a704a92a4bedee5e390a00ad4ea
liangxp <liangxp@ifeng.com>
2020-05-13 17:32:03 +0800

    volatile可见性测试

---
+++ jvm_study/src/com/jvm/gc/T01_ThreadVisibility.java
@@ -0,0 +1,28 @@
+package com.jvm.gc;
+
+/**
+ * @ClassName T01_ThreadVisibility
+ * @Description volatile 可见性测试
+ * @Author liangxp
+ * @Date 2020/5/13 17:27
+ **/
+public class T01_ThreadVisibility {
+
+    private static /*volatile */boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() ->{
+            while (flag){
+
+            }
+            System.out.println("end");
+
+        }).start();
+
+        Thread.sleep(1000L);
+
+        flag = false;
+
+
+    }
+}
