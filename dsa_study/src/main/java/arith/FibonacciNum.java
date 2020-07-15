3ad089ce787bb1b1793c4321a5741204485c8a2b
liangxp <liangxp@ifeng.com>
2020-07-15 18:06:49 +0800

    斐波那契数列输出

---
+++ dsa_study/src/main/java/arith/FibonacciNum.java
@@ -0,0 +1,28 @@
+package arith;
+
+/**
+ * @ClassName FibonacciNum
+ * @Description 获取斐波那契数列
+ * @Author liangxp
+ * @Date 2020/7/14 11:23
+ **/
+public class FibonacciNum {
+    public static void main(String[] args) {
+
+        int x = 20;
+        for (int i = 1; i < x; i++) {
+            System.out.println(fibonacc(i));
+        }
+    }
+
+    private static int fibonacc(int n){
+        if (n == 1){
+            return 0;
+        }
+        if (n == 2){
+           return 1;
+        }
+        int num = fibonacc(n - 2) + fibonacc(n - 1);
+        return num;
+    }
+}
